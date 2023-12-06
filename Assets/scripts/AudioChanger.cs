4c45b4d4ef2053d7e2a86aa4587ab62a2a4e8c7a
Mr-oxm <88877318+Mr-oxm@users.noreply.github.com>
2023-12-06 21:40:45 +0200

    updated audio manager

--- Assets/scripts/AudioChanger.cs
+++ Assets/scripts/AudioChanger.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class AudioChanger : MonoBehaviour
 {
-    public int choice; // "bgMusic", "bgEffectsOne", "bgEffectsTwo", "layla"
+    public int choice; // "bgMusic", "bgEffectsOne", "bgEffectsTwo", "layla", "bgEffectsThree", "Aziz", "Salim", "Dad", "Mom"
     public int index; // Index of the audio clip
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,7 +25,23 @@ public class AudioChanger : MonoBehaviour
                         audioManager.PlayBgEffectsTwo(index);
                         break;
                     case 3:
+                        audioManager.PlayBgEffectsThree(index);
+                        break;
+                    // New cases for additional categories
+                    case 4:
                         audioManager.PlayLayla(index);
+                        break;
+                    case 5:
+                        audioManager.PlayAziz(index);
+                        break;
+                    case 6:
+                        audioManager.PlaySalim(index);
+                        break;
+                    case 7:
+                        audioManager.PlayDad(index);
+                        break;
+                    case 8:
+                        audioManager.PlayMom(index);
                         break;
                     default:
                         Debug.LogError("Invalid choice in AudioChanger");
--- Assets/scripts/AudioManager.cs
+++ Assets/scripts/AudioManager.cs
@@ -5,12 +5,26 @@ public class AudioManager : MonoBehaviour
     public AudioSource bgMusicSource;
     public AudioSource bgEffectsOneSource;
     public AudioSource bgEffectsTwoSource;
+    public AudioSource bgEffectsThreeSource;
     public AudioSource laylaSource;
+
+    // New Audio Sources
+    public AudioSource AzizSource;
+    public AudioSource SalimSource;
+    public AudioSource DadSource;
+    public AudioSource MomSource;
 
     public AudioClip[] bgMusic;
     public AudioClip[] bgEffectsOne;
     public AudioClip[] bgEffectsTwo;
+    public AudioClip[] bgEffectsThree;
     public AudioClip[] layla;
+
+    // New AudioClip arrays
+    public AudioClip[] Aziz;
+    public AudioClip[] Salim;
+    public AudioClip[] Dad;
+    public AudioClip[] Mom;
 
     // Function to play background music
     public void PlayBgMusic(int index)
@@ -34,6 +48,36 @@ public class AudioManager : MonoBehaviour
     public void PlayLayla(int index)
     {
         SetAudioClipAndPlay(laylaSource, layla, index);
+    }
+
+    // Function to play background effects three
+    public void PlayBgEffectsThree(int index)
+    {
+        SetAudioClipAndPlay(bgEffectsThreeSource, bgEffectsThree, index);
+    }
+
+    // Function to play Aziz
+    public void PlayAziz(int index)
+    {
+        SetAudioClipAndPlay(AzizSource, Aziz, index);
+    }
+
+    // Function to play Salim
+    public void PlaySalim(int index)
+    {
+        SetAudioClipAndPlay(SalimSource, Salim, index);
+    }
+
+    // Function to play Dad
+    public void PlayDad(int index)
+    {
+        SetAudioClipAndPlay(DadSource, Dad, index);
+    }
+
+    // Function to play Mom
+    public void PlayMom(int index)
+    {
+        SetAudioClipAndPlay(MomSource, Mom, index);
     }
 
     // Set audio clip of AudioSource and play
